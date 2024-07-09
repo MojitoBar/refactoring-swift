78107dc91bbb292de55b8d29645e52f928a49f78
MojitoBar <wnehdtjr5@gmail.com>
2024-07-09 21:47:23 +0900

    refactor: totalAmount 변수 인라인화

--- RefactoringSwift/Theater/TheaterMain.swift
+++ RefactoringSwift/Theater/TheaterMain.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 func statement(invoice: Invoice, plays: [String: Play]) -> String {
-    let totalAmount = totalAmount()
     var result = "청구 내역 (고객명: \(invoice.customer))\n"
     
     func usd(_ number: Int) -> String {
@@ -17,7 +16,7 @@ func statement(invoice: Invoice, plays: [String: Play]) -> String {
         result += "\(playFor(perf).name): \(usd(amountFor(perf))) (\(perf.audience)석)\n"
     }
     
-    result += "총액: \(usd(totalAmount))\n"
+    result += "총액: \(usd(totalAmount()))\n"
     result += "적립 포인트: \(totalVolumeCredits())점\n"
     return result
     
