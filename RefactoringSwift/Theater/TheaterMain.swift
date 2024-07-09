31bbdf03e42a8eeb0e5c0ec30701fec5be27f296
MojitoBar <wnehdtjr5@gmail.com>
2024-07-09 22:44:08 +0900

    refactor: 중첩함수 분리

--- RefactoringSwift/Theater/TheaterMain.swift
+++ RefactoringSwift/Theater/TheaterMain.swift
@@ -1,12 +1,35 @@
 import Foundation
 
 func statement(invoice: Invoice, plays: [String: Play]) -> String {
+    return renderPlainText(data: createStatementData(invoice: invoice, plays: plays), plays: plays)
+}
+
+func renderPlainText(data: StatementData, plays: [String: Play]) -> String {
+    var result = "청구 내역 (고객명: \(data.customer))\n"
+    for perf in data.performances {
+        result += "\(perf.play!.name): \(usd(perf.amount!)) (\(perf.audience)석)\n"
+    }
+    result += "총액: \(usd(data.totalAmount!))\n"
+    result += "적립 포인트: \(data.totalVolumeCredits!)점\n"
+    return result
+    
+    func usd(_ number: Int) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = Locale(identifier: "en_US")
+        numberFormatter.minimumFractionDigits = 2
+        return numberFormatter.string(from: NSNumber(value: Double(number) / 100.0)) ?? ""
+    }
+}
+
+func createStatementData(invoice: Invoice, plays: [String: Play]) -> StatementData {
     var statementData: StatementData = StatementData()
     statementData.customer = invoice.customer
     statementData.performances = invoice.performances.map(enrichPerformance(aPerformance:))
     statementData.totalAmount = totalAmount(data: statementData)
     statementData.totalVolumeCredits = totalVolumeCredits(data: statementData)
-    return renderPlainText(data: statementData, plays: plays)
+    return statementData
+    
     
     func enrichPerformance(aPerformance: Performance) -> Performance {
         var result = aPerformance
@@ -55,23 +78,5 @@ func statement(invoice: Invoice, plays: [String: Play]) -> String {
     
     func totalAmount(data: StatementData) -> Int {
         return data.performances.reduce(0) { $0 + ($1.amount ?? 0) }
-    }
-}
-
-func renderPlainText(data: StatementData, plays: [String: Play]) -> String {
-    var result = "청구 내역 (고객명: \(data.customer))\n"
-    for perf in data.performances {
-        result += "\(perf.play!.name): \(usd(perf.amount!)) (\(perf.audience)석)\n"
-    }
-    result += "총액: \(usd(data.totalAmount!))\n"
-    result += "적립 포인트: \(data.totalVolumeCredits!)점\n"
-    return result
-    
-    func usd(_ number: Int) -> String {
-        let numberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .currency
-        numberFormatter.locale = Locale(identifier: "en_US")
-        numberFormatter.minimumFractionDigits = 2
-        return numberFormatter.string(from: NSNumber(value: Double(number) / 100.0)) ?? ""
     }
 }
