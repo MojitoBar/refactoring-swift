a6ae655d0b481e35ee2ebdeb01de40f6d1031188
MojitoBar <wnehdtjr5@gmail.com>
2024-07-09 21:16:23 +0900

    refactor: thisAmount 명확한 이름으로 변경

--- RefactoringSwift/Theater/TheaterMain.swift
+++ RefactoringSwift/Theater/TheaterMain.swift
@@ -33,23 +33,23 @@ func statement(invoice: Invoice, plays: [String: Play]) -> String {
     result += "적립 포인트: \(volumeCredits)점\n"
     return result
     
-    func amountFor(_ perf: Performance, _ play: Play) -> Int {
-        var thisAmount = 0
+    func amountFor(_ aPerformance: Performance, _ play: Play) -> Int {
+        var result = 0
         switch play.type {
         case "tragedy": // 비극
-            thisAmount = 40000
-            if perf.audience > 30 {
-                thisAmount += 1000 * (perf.audience - 30)
+            result = 40000
+            if aPerformance.audience > 30 {
+                result += 1000 * (aPerformance.audience - 30)
             }
         case "comedy": // 희극
-            thisAmount = 30000
-            if perf.audience > 20 {
-                thisAmount += 10000 + 500 * (perf.audience - 20)
+            result = 30000
+            if aPerformance.audience > 20 {
+                result += 10000 + 500 * (aPerformance.audience - 20)
             }
-            thisAmount += 300 * perf.audience
+            result += 300 * aPerformance.audience
         default:
             fatalError("알 수 없는 장르: \(play.type)")
         }
-        return thisAmount
+        return result
     }
 }
